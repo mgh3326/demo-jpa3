cec614fe30890270cda04cc1e4629d81e2622e0d
kwanghyunMoon <mgh3326@naver.com>
2019-07-22 17:57:19 +0900

    first update

---
+++ src/main/java/me/khmoon/demojpa3/DemoJpa3Application.java
@@ -0,0 +1,13 @@
+package me.khmoon.demojpa3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJpa3Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoJpa3Application.class, args);
+  }
+
+}
